0717e50400097cc84cd5b719cfdfc60d6c23b409
danielduyvo <38112586+danielduyvo@users.noreply.github.com>
2018-10-10 15:23:54 -0700

    Eden made the buttons not ugly

--- apo-x.org/people/myrosterinfo.php
+++ apo-x.org/people/myrosterinfo.php
@@ -237,9 +237,16 @@ extract($line);
 </form> 
 </table>
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload (send in as .jpg/.jpeg):
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    Select an image to upload (must be .jpg/.jpeg format also pls be PG-13):
+	<br>
+    <input type="file" name="fileToUpload" id="fileToUpload" class="inputfile"/>
+		<label for="file-3">
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17">
+				<path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/>
+			</svg> 
+			<span>Choose a file&hellip;</span>
+		</label>
     <input type="submit" value="Upload Image" name="submit">
 </form>
-
+<script src="js/custom-file-input.js"></script>
 <?php show_footer(); ?>
